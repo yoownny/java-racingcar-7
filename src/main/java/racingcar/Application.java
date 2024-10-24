140304be491cef1170edba8374758a31cc425104
yoownny <kelosy00@naver.com>
2024-10-24 16:45:11 +0900

    faet(Application): Create default input output
    - "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" 메시지 출력
    - 문자열 입력 받기
    - "시도할 횟수는 몇 회인가요?" 메시지 출력
    - 정수 값 입력 받기
    - "최종 우승자 : [이름]" 형식으로 메시지 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNames = readLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int number = Integer.parseInt(readLine());
+
+        // 임시 값
+        String winner = "pobi";
+        System.out.println("최종 우승자 : " + winner);
     }
 }
