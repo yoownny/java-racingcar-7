766e700a4aed7933ec346b3782735ee9e4ca28bd
yoownny <kelosy00@naver.com>
2024-10-25 21:57:13 +0900

    refactor(RacingGame): Modify code structure
    - 프로그래밍 요구 사항 2에 맞게 indent depth가 3이 넘지 않도록 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -17,31 +17,54 @@ public class RacingGame {
         this.attempts = attempts;
     }
 
+    // 게임 실행
     public void start() {
         System.out.println("실행 결과");
         for (int i = 0; i < attempts; i++) {
-            for (Car car : cars) {
-                car.move(Randoms.pickNumberInRange(0, 9));
-                System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
-            }
+            moveAllCars();
+            printRaceStatus();
             System.out.println();
         }
     }
 
-    public String[] getWinners() {
-        int max = 0;
-
+    private void moveAllCars() {
         for (Car car : cars) {
-            max = Math.max(max, car.getPosition());
+            car.move(Randoms.pickNumberInRange(0, 9));
         }
+    }
 
+    private void printRaceStatus() {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+    }
+
+    // 우승자
+    public String[] getWinners() {
+        int max = findMaxPosition();
+        int winnerCount = findCarNumber(max);
+        return sendWinners(max, winnerCount);
+    }
+
+    private int findCarNumber(int max) {
         int winnerCount = 0;
         for (Car car : cars) {
             if (car.getPosition() == max) {
                 winnerCount++;
             }
         }
+        return winnerCount;
+    }
 
+    private int findMaxPosition() {
+        int max = 0;
+        for (Car car : cars) {
+            max = Math.max(max, car.getPosition());
+        }
+        return max;
+    }
+
+    private String[] sendWinners(int max, int winnerCount) {
         String[] winners = new String[winnerCount];
         int i = 0;
         for (Car car : cars) {
@@ -50,7 +73,6 @@ public class RacingGame {
                 i++;
             }
         }
-
         return winners;
     }
 }
