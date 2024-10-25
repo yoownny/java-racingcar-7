d36c7535f0126261d814b644269aa74d44146d93
yoownny <kelosy00@naver.com>
2024-10-26 00:45:53 +0900

    test(RacingGameTest): Create RacingGameTest
    - 정상적인 이름으로 게임 실행 테스트
    - 빈 문자열이 포함된 이름으로 게임 실행 테스트
    - 공백이 포함된 이름으로 게임 실행 테스트

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,7 +1,39 @@
 package racingcar;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 
 class RacingGameTest {
+    @Test
+    @DisplayName("정상적인 이름으로 게임 실행")
+    void valid_names() {
+        String[] names = {"pobi", "woni", "jun"};
+        RacingGame game = new RacingGame(names, 5);
+        game.start();
+        String[] winners = game.getWinners();
+        assertThat(winners).isNotNull();
+    }
 
+    @Test
+    @DisplayName("빈 문자열이 포함된 이름으로 게임 실행")
+    void empty_name() {
+        String[] names = {"pobi", "", "jun"};
+        RacingGame game = new RacingGame(names, 5);
+        game.start();
+        String[] winners = game.getWinners();
+        assertThat(winners).isNotNull();
+    }
+
+    @Test
+    @DisplayName("공백이 포함된 이름으로 게임 실행")
+    void spaced_names() {
+        String[] names = {" pobi ", "woni", " jun "};
+        RacingGame game = new RacingGame(names, 5);
+        game.start();
+        String[] winners = game.getWinners();
+        assertThat(winners).isNotNull();
+    }
 }
